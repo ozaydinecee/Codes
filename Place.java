504b3fefddb26762064038ef10e066ae24a319d7
ozaydinecee <ozaydinecee@gmail.com>
2021-01-17 15:09:04 +0300

    added place.java

---
+++ Place.java
@@ -0,0 +1,13 @@
+package assignment3;
+
+public class Place {
+	public int placeType;
+	public double x, y;
+	Place(){}
+	
+	Place(int placeType, double x, double y){
+		this.placeType = placeType;
+		this.x = x;
+		this.y = y;
+	}
+}
